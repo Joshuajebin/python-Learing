612a67979a806f0c59e7c6920adcd001c21588d1
Joshuajebin <131939690+Joshuajebin@users.noreply.github.com>
2024-09-10 15:19:29 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,12 @@
+name = 'sun mon tues wednes thurs fri'
+l =  name.split(' ')
+print(l)
+for day in l:
+    print(day+'day', end=' ')
+       #0123
+#name.insert('i',3)
+print(name)
+
+#split() is a function, from
+#str     list    int
+
